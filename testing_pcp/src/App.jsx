d58736b4ed929cef0b6abc086f1db925b2ae9673
Yuvaraj” <yuvarajoffical06@gmail.com>
2026-04-21 09:57:29 +0530

    Remove duplicate testing_pcp folder

--- testing_pcp/src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router/router";
-import "./App.css";
-
-function App() {
-  return <RouterProvider router={router} />;
-}
-
-export default App;
